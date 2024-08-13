e491405b5da83d3795635bf3bb90d422b2a9e849
Felipe Marcelino <felipe.marcelino1991@gmail.com>
2024-08-12 22:01:45 -0300

    fix

--- flake.nix
+++ flake.nix
@@ -11,18 +11,33 @@
     flake-parts.follows = "emanote/flake-parts";
   };
 
-  outputs = inputs@{ self, flake-parts, nixpkgs, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs = inputs @ {
+    self,
+    flake-parts,
+    nixpkgs,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
       systems = nixpkgs.lib.systems.flakeExposed;
-      imports = [ inputs.emanote.flakeModule ];
-      perSystem = { self', pkgs, system, ... }: {
+      imports = [inputs.emanote.flakeModule];
+      perSystem = {
+        self',
+        pkgs,
+        system,
+        ...
+      }: {
         emanote = {
           # By default, the 'emanote' flake input is used.
           # package = inputs.emanote.packages.${system}.default;
           sites."default" = {
-            layers = [{ path = ./.; pathString = "."; }];
+            layers = [
+              {
+                path = ./.;
+                pathString = ".";
+              }
+            ];
             # port = 8080;
-            baseUrl = "/emanote-template/"; # Change to "/" (or remove it entirely) if using CNAME
+            baseUrl = ""; # Change to "/" (or remove it entirely) if using CNAME
             # prettyUrls = true;
           };
         };
