e2a401cc386d9a55c410168f97b5d299c49c89b6
Felipe Marcelino <felipe.marcelino1991@gmail.com>
2024-08-12 21:27:41 -0300

    last time today

--- flake.nix
+++ flake.nix
@@ -1,68 +1,37 @@
 {
+  nixConfig = {
+    extra-substituters = "https://cache.garnix.io";
+    extra-trusted-public-keys = "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=";
+  };
+
   inputs = {
-    emanote.url = "github:srid/emanote/32666807bc2966c796eb95364716dfb08c50cefe";
-    nixpkgs.url = "github:NixOS/nixpkgs/3665c429d349fbda46b0651e554cca8434452748";
+    emanote.url = "github:srid/emanote";
+    emanote.inputs.emanote-template.follows = "";
+    nixpkgs.follows = "emanote/nixpkgs";
+    flake-parts.follows = "emanote/flake-parts";
   };
 
-  outputs = inputs: let
-    emanote = inputs.emanote.packages.x86_64-linux.default;
-    pkgs = import inputs.nixpkgs {system = "x86_64-linux";};
-    ebml = pkgs.fetchFromGitHub {
-      owner = "TristanCacqueray";
-      repo = "haskell-ebml";
-      rev = "aff25512b52e48e92d77cd59019a0291a8b43bf4";
-      sha256 = "sha256-U2Mo83gr7dLm+rRKOLzS9LZUaZ90ECO6Zjbv6maflyc=";
+  outputs = inputs@{ self, flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [ inputs.emanote.flakeModule ];
+      perSystem = { self', pkgs, system, ... }: {
+        emanote = {
+          # By default, the 'emanote' flake input is used.
+          # package = inputs.emanote.packages.${system}.default;
+          sites."default" = {
+            layers = [{ path = ./.; pathString = "."; }];
+            # port = 8080;
+            baseUrl = "/emanote-template/"; # Change to "/" (or remove it entirely) if using CNAME
+            # prettyUrls = true;
+          };
+        };
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nixpkgs-fmt
+          ];
+        };
+        formatter = pkgs.nixpkgs-fmt;
+      };
     };
-    ghc = pkgs.haskellPackages.ghcWithPackages (p: [
-      p.markdown-unlit
-      p.rio
-      p.string-qq
-      p.ki
-      p.servant
-      p.servant-websockets
-      p.servant
-      p.lucid
-      p.servant-lucid
-      p.websockets
-      p.yaml
-      p.pandoc
-      p.pandoc-types
-      (pkgs.haskellPackages.callCabal2nix "ebml" ebml {})
-    ]);
-    website = pkgs.stdenv.mkDerivation {
-      name = "felipemarcelino.io-pages";
-      buildInputs = [emanote];
-      src = inputs.self;
-      # https://github.com/jaspervdj/hakyll/issues/614
-      # https://github.com/NixOS/nix/issues/318#issuecomment-52986702
-      # https://github.com/MaxDaten/brutal-recipes/blob/source/default.nix#L24
-      LOCALE_ARCHIVE =
-        pkgs.lib.optionalString (pkgs.buildPlatform.libc == "glibc")
-        "${pkgs.glibcLocales}/lib/locale/locale-archive";
-      LANG = "en_US.UTF-8";
-
-      buildPhase = ''
-        mkdir _out
-        emanote -L content/ gen _out
-      '';
-      installPhase = ''
-        mv _out $out
-      '';
-    };
-    run = pkgs.writeScriptBin "run" ''
-      ${emanote}/bin/emanote -L content/ run --host 0.0.0.0 --port 8080
-    '';
-  in {
-    packages.x86_64-linux.default = website;
-    apps."x86_64-linux".default = {
-      type = "app";
-      program = "${run}/bin/run";
-    };
-    devShells."x86_64-linux".default =
-      pkgs.mkShell {buildInputs = [ghc pkgs.ghcid emanote];};
-    devShells."x86_64-linux".gstreamer = pkgs.mkShell {
-      buildInputs = [ghc pkgs.ghcid pkgs.gst_all_1.gstreamer];
-      GST_PLUGIN_PATH = "${pkgs.gst_all_1.gst-plugins-base}/lib/gstreamer-1.0/:${pkgs.gst_all_1.gst-plugins-good}/lib/gstreamer-1.0/";
-    };
-  };
 }
