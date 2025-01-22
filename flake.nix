8e93c14d164cbed4718505ede72f3df8039fe25b
Felipe Marcelino <felipe.marcelino1991@gmail.com>
2025-01-22 07:45:13 -0300

    commitment

--- flake.nix
+++ flake.nix
@@ -40,7 +40,7 @@
                 }
               ];
               # port = 8080;
-              #baseUrl = "/emanote-template/"; # Change to "/" (or remove it entirely) if using CNAME
+              baseUrl = "/";
               allowBrokenLinks = true;
               prettyUrls = true;
             };
